1c87be7a07ef6fe39586a6a0b9a72ee6e2c0307e
hawkazizi <hawk.azizi1@gmail.com>
2025-08-06 20:17:25 +0330

    update

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = 5002;
 const app = express();
 app.use(cors());
 app.use("/api/admin", systemInfoRoutes);
+app.use("/", (req, res) => res.send("<h1>hi</h1>"));
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
